cdd2f5ff2b0345cad78f974c3f306855b1d36706
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-01 12:21:09 +0600

    Function procdcures

--- functionsProcedures.sql
+++ functionsProcedures.sql
@@ -19,3 +19,20 @@ $$
 $$
 
 SELECT delete_emp_by_id(29);
+
+
+CREATE Procedure remove_emp(p_emp_id int)
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+    test_var INT 
+    BEGIN
+        SELECT employee_id INTO test_var from  employees WHERE employees_id = p_emp_id;
+        DELETE FROM employees WHERE employee_id = test_var;
+
+        RAISE NOTICE 'Employee removed successfully!'
+    END
+$$;
+
+CALL remove_emp(25);
